0bb93507d08454410eb9e3138a2a36f22e58104c
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-02-22 23:37:04 -0500

    Work In Progress

--- packages/manager/src/components/Button/Button.tsx
+++ packages/manager/src/components/Button/Button.tsx
@@ -1,121 +1,94 @@
-import classNames from 'classnames';
-import { always, cond, propEq } from 'ramda';
 import * as React from 'react';
 import Reload from 'src/assets/icons/reload.svg';
-import _Button, { ButtonProps } from 'src/components/core/Button';
-import { makeStyles, Theme } from 'src/components/core/styles';
+import _Button, { ButtonProps } from '@mui/material/Button';
+import { keyframes } from 'tss-react';
+import { styled, useTheme } from '@mui/material/styles';
+import { SxProps } from '@mui/system';
 import HelpIcon from 'src/components/HelpIcon';
 
 export interface Props extends ButtonProps {
   buttonType?: 'primary' | 'secondary' | 'outlined';
-  className?: string;
+  sx?: SxProps;
   compactX?: boolean;
   compactY?: boolean;
   loading?: boolean;
   tooltipText?: string;
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  '@keyframes rotate': {
-    from: {
-      transform: 'rotate(0deg)',
-    },
-    to: {
-      transform: 'rotate(360deg)',
-    },
-  },
-  loading: {
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const BaseButton = styled(_Button, {
+  shouldForwardProp: (prop) =>
+    prop !== 'buttonType' &&
+    prop !== 'compactX' &&
+    prop !== 'compactY' &&
+    prop !== 'loading',
+})<Props>(({ theme, buttonType, compactX, compactY, loading }) => ({
+  ...(buttonType === 'secondary' &&
+    compactX && { minWidth: 50, paddingRight: 0, paddingLeft: 0 }),
+  ...(buttonType === 'secondary' &&
+    compactY && { minHeight: 20, paddingTop: 0, paddingBottom: 0 }),
+  ...(loading && {
     '& svg': {
-      animation: '$rotate 2s linear infinite',
+      animation: `${rotate} 2s linear infinite`,
       margin: '0 auto',
-      height: `${theme.spacing(2)} !important`,
-      width: `${theme.spacing(2)} !important`,
+      height: `${theme.spacing(2)}`,
+      width: `${theme.spacing(2)}`,
     },
-  },
-  compactX: {
-    minWidth: 50,
-    paddingRight: 0,
-    paddingLeft: 0,
-  },
-  compactY: {
-    minHeight: 20,
-    paddingTop: 0,
-    paddingBottom: 0,
-  },
-  reg: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  '@supports (-moz-appearance: none)': {
-    /* Fix text alignment for Firefox */
-    reg: {
-      marginTop: 2,
-    },
-  },
-  helpIcon: {
-    marginLeft: `-${theme.spacing()}`,
-  },
+  }),
 }));
 
-type CombinedProps = Props;
+const Span = styled('span')({
+  display: 'flex',
+  alignItems: 'center',
+  '@supports (-moz-appearance: none)': {
+    /* Fix text alignment for Firefox */
+    marginTop: 2,
+  },
+});
 
-const getVariant = cond([
-  [propEq('buttonType', 'primary'), always('contained')],
-  [propEq('buttonType', 'secondary'), always('contained')],
-  [propEq('buttonType', 'outlined'), always('outlined')],
-  [() => true, always(undefined)],
-]);
-
-const getColor = cond([
-  [propEq('buttonType', 'primary'), always('primary')],
-  [propEq('buttonType', 'secondary'), always('secondary')],
-  [() => true, always(undefined)],
-]);
-
-export const Button: React.FC<CombinedProps> = (props) => {
-  const classes = useStyles();
-
-  const {
-    buttonType,
-    className,
-    compactX,
-    compactY,
-    loading,
-    tooltipText,
-    ...rest
-  } = props;
+export const Button = ({
+  buttonType,
+  children,
+  compactX,
+  compactY,
+  disabled,
+  loading,
+  sx,
+  tooltipText,
+  ...rest
+}: Props) => {
+  const theme = useTheme();
+  const color = buttonType === 'primary' ? 'primary' : 'secondary';
+  const variant =
+    buttonType === 'primary' || buttonType === 'secondary'
+      ? 'contained'
+      : 'outlined';
+  const sxHelpIcon = {
+    marginLeft: `-${theme.spacing()}`,
+  };
 
   return (
     <React.Fragment>
-      <_Button
+      <BaseButton
         {...rest}
-        className={classNames(
-          buttonType,
-          {
-            [classes.compactX]: buttonType === 'secondary' ? compactX : false,
-            [classes.compactY]: buttonType === 'secondary' ? compactY : false,
-            [classes.loading]: loading,
-            disabled: props.disabled,
-            loading,
-          },
-          className
-        )}
-        color={getColor(props)}
-        disabled={props.disabled || loading}
-        variant={getVariant(props)}
+        color={color}
+        compactX={compactX}
+        compactY={compactY}
+        disabled={disabled || loading}
+        sx={sx}
+        variant={variant}
       >
-        <span
-          className={classNames({
-            [classes.reg]: true,
-          })}
-          data-qa-loading={loading}
-        >
-          {loading ? <Reload /> : props.children}
-        </span>
-      </_Button>
-      {tooltipText && (
-        <HelpIcon className={classes.helpIcon} text={tooltipText} />
-      )}
+        <Span data-qa-loading={loading}>{loading ? <Reload /> : children}</Span>
+      </BaseButton>
+      {tooltipText && <HelpIcon sx={sxHelpIcon} text={tooltipText} />}
     </React.Fragment>
   );
 };
